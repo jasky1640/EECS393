03fac77c9069c9619f4923292e96243d4c34686b
Tao <49325650+txh325@users.noreply.github.com>
2019-11-01 10:37:18 -0400

    keep adding

--- Web Page/admin/webTest.js
+++ Web Page/admin/webTest.js
@@ -1,9 +1,25 @@
+// app.test.js
 
-var script = require('../script.js');
+var app = require('../script.js');
 var assert = require('assert');   // use Node.js itself assert module
 
-describe('test onFormSubmit function in script.js', function() {
-  it('Should call resetForm function', function() {
-    assert.equal(script(), resetForm());
+describe('test functions in script.js', function() {
+  it('The output data should the same as the input data', function() {
+	assert.equal(readFormData(),{"className":"testClassName","classTime":"testClassTime","professor":"testProfessor","information":"testInformation"});
   });
+  it('After the insertion of new record there should be a new row', () => {
+	assert.equal(insertNewRecord(), {"before_insert":0,"after_insert":1});
+  });
+   it('The data should be clear after reset', () => {
+	assert.equal(insertNewRecord(), {"className":"","classTime":"","professor":"","information":""}));
+  });
+   it('The data should be changed', () => {
+	assert.equal(onEdit(1), {"className":"onEditClassName","classTime":"onEditClassTime","professor":"onEditProfessor","information":"onEditInformation"}));
+  });
+  it('It will update the new information of the data', () => {
+	assert.equal(updateRecord(), {"className":"afterUpdateClassName","classTime":"afterUpdateClassTime","professor":"afterUpdateProfessor","information":"afterUpdateInformation"}));
+  });
+  
+
+
 });
